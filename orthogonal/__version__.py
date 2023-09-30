a82286b14fcbee3bd4ecbc0b4515723d97f7975a
Caceresenzo <caceresenzo1502@gmail.com>
2023-09-30 20:11:41 +0200

    feat(package): add @matteoettam09 as an author

--- orthogonal/__version__.py
+++ orthogonal/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'orthogonal-cli'
 __description__ = 'orthogonal-cli - CLI for the DataCrunch Tournament by CrunchDAO'
 __version__ = '0.3.0'
-__author__ = 'Enzo CACERES'
+__author__ = 'Enzo CACERES, Matteo MANZI'
 __author_email__ = 'enzo.caceres@crunchdao.com'
 __url__ = 'https://github.com/crunchdao/orthogonal-cli'
